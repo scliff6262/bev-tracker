c632b74fb63c3fd1461c4f58221d272bfe7cc947
scliff6262 <sclifford6262@gmail.com>
2018-03-09 16:38:45 -0500

    Create application_controller.rb

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+require './config/environment'
+
+
+class AppController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+  
+end
