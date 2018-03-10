4157bbea3e1b4e0bb44372b3ff98a880ce7083ce
Sean Clifford <sclifford6262@gmail.com>
2018-03-10 19:58:52 +0000

    added user show page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,18 +8,17 @@ class UserController < AppController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect "/user/#{@user.id}"
+      redirect "/users/#{@user.id}"
     else
       redirect "/login"
     end
   end
 
   get '/signup' do
-    binding.pry
     unless logged_in?
       erb :"users/new"
     else
-      redirect "/user/#{this_user.id}"
+      redirect "/users/#{this_user.id}"
     end
   end
 
@@ -29,13 +28,17 @@ class UserController < AppController
 
     if @user.save
       session[:user_id] = @user.id
-      redirect "/user/#{@user.id}"
+      redirect "/users/#{@user.id}"
     else
       redirect "/signup"
     end
   end
 
-  post '/user/:id/logout' do
+  get '/users/:id' do
+    erb :"/users/show"
+  end
+
+  post '/users/:id/logout' do
     logout
     redirect '/'
   end
