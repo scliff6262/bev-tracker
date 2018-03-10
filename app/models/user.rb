3051722eee5ffc453d21e7cd01c0a8d914eddd9d
Sean Clifford <sclifford6262@gmail.com>
2018-03-10 20:26:07 +0000

    added validations and username uniqueness to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates :username, :presence => true, :uniqueness => true
+  validates :password, :presence => true
+  
   has_many :beverages
   has_secure_password
-end 
+end
