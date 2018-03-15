6b9226cdee1686d820236f5760eae49d60d3336f
Sean Clifford <sclifford6262@gmail.com>
2018-03-15 20:56:06 +0000

    added validations for name, category and rating BEV model

--- app/models/beverage.rb
+++ app/models/beverage.rb
@@ -1,3 +1,7 @@
 class Beverage < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :category, :presence => true
+  validates :rating, :presence => true 
+
   belongs_to :user
-end 
+end
