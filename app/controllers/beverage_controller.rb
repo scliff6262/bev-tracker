95405df6a2e5a2c2f771cd434d3f41b3079c7d17
Sean Clifford <sclifford6262@gmail.com>
2018-03-15 16:56:05 +0000

    added post beverages/new

--- app/controllers/beverage_controller.rb
+++ app/controllers/beverage_controller.rb
@@ -1,9 +1,17 @@
 class BeverageController < AppController
 
-
   get '/beverages/new' do
-  
+
     erb :'beverages/new'
+  end
+
+  post '/beverages/new' do
+    if this_user.beverages.build(params).save
+      @bev = this_user.beverages.last
+      redirect "/beverages/#{@bev.id}"
+    else
+      redirect "/beverages/new"
+    end
   end
 
   get '/beverages/:id' do
