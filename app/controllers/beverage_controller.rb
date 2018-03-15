ce07e18e84d73d1f832f6ab41be1e4d5703f9ef6
Sean Clifford <sclifford6262@gmail.com>
2018-03-15 16:29:09 +0000

    added 'new' and 'show' pages

--- app/controllers/beverage_controller.rb
+++ app/controllers/beverage_controller.rb
@@ -1,2 +1,14 @@
 class BeverageController < AppController
-end 
+
+
+  get '/beverages/new' do
+  
+    erb :'beverages/new'
+  end
+
+  get '/beverages/:id' do
+    @bev = Beverage.find(params[:id])
+    erb :"/beverages/show"
+  end
+
+end
